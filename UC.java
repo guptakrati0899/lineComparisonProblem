703d51d15c9667a3f73aa9403d9b2bc19196748c
KratiGupta <kratigupta8545@gmail.com>
2021-07-23 12:42:15 +0530

    Added UC02 branch successfully

--- UC.java
+++ UC.java
@@ -20,6 +20,30 @@ public class UC {
 	double length1=Math.ceil(Math.sqrt((x2-x1)^2+(y2-y1)^2));
 	System.out.println(length1);
 	
+	
+	//UC2
+	
+	
+	int a1=1;
+	int b1=2;
+			
+	int a2=1;
+	int b2=5;
+			
+	double length2=Math.ceil(Math.sqrt((a2-a1)^2+(b2-b1)^2));
+	System.out.println(length2);
+			
+	String s1=String.valueOf(length1);  
+	String s2=String.valueOf(length2);  
+			
+			
+	if(s1.equals(s2)) {
+			System.out.println("Both are equal");
+		}else {
+			System.out.println("not equal");
+		}
+			
+	
 	}
 	
 }
