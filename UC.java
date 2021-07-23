c5e339facfa5ec04ac7f964643b4eb7dfe0ffa37
KratiGupta <kratigupta8545@gmail.com>
2021-07-23 12:30:00 +0530

    Added Welcome Message Successfully

---
+++ UC.java
@@ -0,0 +1,15 @@
+package lineComparisonProblem;
+import java.lang.Math;
+import java.lang.Double;
+
+
+
+public class UC {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to line Comparison Problem on Master Branch");
+	}
+}
+		
+	
+
