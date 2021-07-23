f231d4ff8e23a02ce1d3364173283622a6065662
KratiGupta <kratigupta8545@gmail.com>
2021-07-23 12:36:39 +0530

    Added UC01 branch successfully

--- UC.java
+++ UC.java
@@ -8,7 +8,20 @@ public class UC {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to line Comparison Problem on Master Branch");
+
+	
+	//UC1
+	int x1=1;
+	int y1=2;
+	
+	int x2=1;
+	int y2=5;
+	
+	double length1=Math.ceil(Math.sqrt((x2-x1)^2+(y2-y1)^2));
+	System.out.println(length1);
+	
 	}
+	
 }
 		
 	
